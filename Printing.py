3d28663e7c2cabc4b034fd245aaedbcbd8351089
oniva7890 <mohaiminasr.@gmail.com>
2026-08-28 13:42:28 +0600

    Add files via upload

---
+++ Printing.py
@@ -0,0 +1,6 @@
+#User Input
+name=input("enter your name:")
+print("\nHello", name , "\nWelcome to Codingal")
+hobby = input("enter your hobby:")
+print("\nOhh! That's great", end="  ")
+print("I also love to do", hobby)
