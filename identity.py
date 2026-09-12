0fd0f1a80fb185ef56c5dc8cfeb74f8bb52abd3f
oniva7890 <mohaiminasr.@gmail.com>
2026-09-12 13:25:08 +0600

    Add files via upload

---
+++ identity.py
@@ -0,0 +1,17 @@
+x = 5
+if(type(x) is int):
+    print("True")
+else:
+    print("False")
+x = 5.5
+if (type(x) is not float):
+    print("True")
+else:
+    print("False")
+x = 20
+y = 20
+if (x is y):
+    print("x & y have the SAME identity")
+y = 30
+if (x is not y):
+    print("x & y don't have the SAME identity")
